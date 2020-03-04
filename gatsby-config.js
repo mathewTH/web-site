72ee05487ad5e53397828f73fe6b2c8e24aa82bf
Colin Henwood <aretenz@users.noreply.github.com>
2020-03-05 12:43:55 +1300

    Replace footer with copyright message only (#23)

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,16 +4,11 @@ require("dotenv").config({
 
 module.exports = {
 	siteMetadata: {
-		title: 'Gatsby Bulma Quickstart',
-		author: 'Aman Mittal',
+		title: 'Manukau Tramping Club',
+		author: 'Colin Henwood',
 		imageUrl: 'https://i.imgur.com/Vz81GEl.png',
 		description: 'A Project to bootstrap your next Gatsby + Bulma site.',
 		keywords: `Web developer, Web, Developer, CSS, HTML, JS, Javascript, Gatsby, Bulma Developer, CSS3, HTML5, Seo, Starter`,
-		twitter: 'https://twitter.com/amanhimself',
-		github: `https://github.com/amandeepmittal`,
-		medium: 'https://medium.com/@amanhimself',
-		gatsby: 'https://www.gatsbyjs.org/',
-		bulma: 'https://bulma.io/',
 		siteUrl: `https://www.example.com`
 	},
 
